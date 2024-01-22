9e224a0c816ef25c9bc01e7ec9c723eb9636340b
Dariia Sliusar <dariiasliusar@gmail.com>
2024-01-22 19:11:06 +0100

    attribute, metodth css

--- jquery2/main.js
+++ jquery2/main.js
@@ -72,3 +72,55 @@
 //     }
 //     widthAndHeight('arrowDown');
 // });
+
+// $(function () {
+    // alert($('.mainText').html());
+    // $('.mainText').html('<b>New Text</b><h2>Text</h2>');
+
+    // $('.mainText').fadeOut(2000).fadeIn(3000);
+
+    // function elementOut(element, time) {
+    //     if (time > 5000 || time < 1000 || isNaN(time)) {
+    //         return false;
+    //     } else {
+    //         var className = "." + element;
+    //         $(className).fadeOut(time);
+    //     }
+    // }
+    // elementOut('mainText', 2000);
+
+//     function newFunc() {
+//         //code
+//     }
+//     $('.mainText').fadeTo(4000, 0.2, newFunc()).fadeTo(2000, 1);
+
+    // $('.mainText').slideUp(2000).slideDown(1000);
+// });
+
+// Attribute
+
+$(function () {
+    // var src = $('.logo img').attr('src', 'icon1.png');
+    // alert(src);
+
+    // $('.logo img').attr('title', 'підказка')
+
+    // $('.logo img').removeAttr('src');
+
+    // function changeAttr(element, newAttr, newValue) {
+    //     var className = "." + element;
+    //     $(className).attr(newAttr, newValue);
+    // }
+    // changeAttr('logo', 'title', 'anything new text');
+
+    // $('nav menu').addClass('border').removeClass('border');
+
+    // alert($('nav menu').css('font-size'));
+    // $('nav menu li a').css('color', 'red');
+    // $('nav menu li a').css('font-size', '25px').css('color', 'green');
+    $('nav menu li a').css({
+        'color': '#ff0000',
+        'font-size': '20px',
+        'padding':'10px'
+    })
+});
